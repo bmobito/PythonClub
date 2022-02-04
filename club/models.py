a764a2676256b09b329d5803c9aa5041c84b7831
T D Rogers <rogers.tdr@gmail.com>
2022-02-03 16:17:53 -0800

    Register first models

--- club/models.py
+++ club/models.py
@@ -1,3 +1,60 @@
+from datetime import date
+from tabnanny import verbose
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Meeting(models.Model):
+    meetingtitle=models.CharField(max_length=255)
+    meetingdate=models.DateField()
+    meetingtime=models.TimeField()
+    meetinglocation=models.CharField(max_length=255)
+    meetingagenda=models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.meetingtitle
+
+    class Meta:
+        db_table='meeting'
+        verbose_name_plural='meetings'
+
+class MeetingMinutes(models.Model):
+    meetingid=models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+    attendance=models.ManyToManyField(User)
+    minutestext=models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.meetingid
+
+    class Meta:
+        db_table='meetingminutes'
+        verbose_name_plural='meetingminutes'
+
+class Resource(models.Model):
+    resourcename=models.CharField(max_length=255)
+    resourcetype=models.CharField(max_length=255)
+    resourceurl=models.URLField()
+    entrydate=models.DateField()
+    userid=models.ForeignKey(User, on_delete=models.CASCADE)
+    description=models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.resourcename
+
+    class Meta:
+        db_table='resource'
+        verbose_name_plural='resources'
+
+class Event(models.Model):
+    eventname=models.CharField(max_length=255)
+    eventlocation=models.CharField(max_length=255)
+    date=models.DateField()
+    time=models.TimeField()
+    description=models.TextField(null=True, blank=True)
+    userid=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.eventname
+
+    class Meta:
+        db_table='event'
+        verbose_name_plural='events'
