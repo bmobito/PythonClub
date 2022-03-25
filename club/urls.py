5cabe7e7ca78500d9e8de8ae8f9ce48133101920
T D Rogers <rogers.tdr@gmail.com>
2022-03-25 15:28:34 -0700

    Assignment 6 Meeting and detail commits

--- club/urls.py
+++ club/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
    path('', views.index, name='index'),
    path('resources/', views.resources, name='resources'),
    path('meetings/', views.meetings, name='meetings'),
-   path('meetingDetails/<int:id>', views.meetingDetails, name='meetingDetails'),
+   path('meetingDetails/<int:id>', views.meetingDetails, name='details'),
 ]
